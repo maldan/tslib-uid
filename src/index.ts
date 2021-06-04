6249172bb0bd9baf3397a299c04f426283fbbdf1
Roman <blackwanted@yandex.ru>
2021-06-04 19:10:38 +0300

    Fix -- Start

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// Hello world
+import SHA1 from 'sha1';
+import * as Os from 'os';
+import { EArray } from '@maldan/tslib-earray';
+
+const USER_STAMP = `${JSON.stringify(
+  Os.userInfo(),
+)}_${Os.version()}_${Os.type()}_${Os.release()}_${Os.platform()}_${Os.hostname()}_${Os.arch()}_${Os.totalmem()}_${JSON.stringify(
+  Os.cpus(),
+)}`;
+
+export function UID(size: number = 12): string {
+  let h = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 4; i++) {
+    h += SHA1(
+      `${Math.random()}_${Math.random()}_sasageo_${new Date().getTime()}_${Os.freemem()}_${USER_STAMP}`,
+    );
+  }
+  return new EArray(h).shuffle().join('').slice(0, size);
+}
--- test/base.test.ts
+++ test/base.test.ts
@@ -1,7 +1,6 @@
 import * as Chai from 'chai';
+import { UID } from '../src';
 
-describe('Base', function() {
-  it('first', async function() {
-    Chai.assert.equal(2 + 2, 4);
-  });
+describe('Base', function () {
+  it('first', async function () {});
 });
